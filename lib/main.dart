9c48e6db06f311a0017d3006389108f01c51c658
MejiaF31 <kababatf@gamil.com>
2025-07-28 10:40:05 -0400

    creando el proyecto

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+void main(){
+  runApp(MyApp());
+
+}
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+
+   return MaterialApp(
+     title:'Sistema R.D.O.T',
+     home: Scaffold(
+       appBar: AppBar(
+
+       ),
+     ),
+
+   );
+  }
+
+}
