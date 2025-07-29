5c9c6d64b1d5c1eff0adc3e55d208f5e2f1f9f69
MejiaF31 <kababatf@gamil.com>
2025-07-28 21:37:43 -0400

    realizando un login basico

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,63 @@
 import 'package:flutter/material.dart';
-void main(){
-  runApp(MyApp());
 
+void main() {
+  runApp(LoginApp());
 }
 
-class MyApp extends StatelessWidget{
+class LoginApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
-   return MaterialApp(
-     title:'Sistema R.D.O.T',
-     home: Scaffold(
-       appBar: AppBar(
-
-       ),
-     ),
-
-   );
+    return MaterialApp(
+      title: 'Login Básico',
+      home: Scaffold(
+        appBar: AppBar(title: Text('Iniciar sesión')),
+        body: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: LoginWidget(),
+        ),
+      ),
+    );
   }
+}
 
+class LoginWidget extends StatefulWidget {
+  @override
+  _LoginWidgetState createState() => _LoginWidgetState();
+}
+
+class _LoginWidgetState extends State<LoginWidget> {
+  String usuario = '';
+  String contrasena = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Text('Nombre de usuario:'),
+        TextField(
+          onChanged: (valor) => usuario = valor,
+        ),
+        SizedBox(height: 10),
+        Text('Contraseña:'),
+        TextField(
+          obscureText: true,
+          onChanged: (valor) => contrasena = valor,
+        ),
+        SizedBox(height: 20),
+        ElevatedButton(
+          onPressed: () {
+            showDialog(
+              context: context,
+              builder: (context) => AlertDialog(
+                title: Text('Bienvenido '),
+                content: Text('Usuario: $usuario\nContraseña: $contrasena'),
+              ),
+            );
+          },
+          child: Text('Iniciar'),
+        ),
+      ],
+    );
+  }
 }
